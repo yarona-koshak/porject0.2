9aa0d6a121d40f584892ef8dabd745e7aa5eb2ff
yarona shneider <s332877679@ariel.ort.org.il>
2024-12-18 08:45:50 +0200

    window
    Co-authored-by: Nekto21-09 <Nekto21-09@users.noreply.github.com>

--- website/pages/login.ts
+++ website/pages/login.ts
@@ -1,14 +1,14 @@
 import { send } from "../utilities";
 
-let username = document.querySelector("#userNameInput")! as HTMLInputElement;
+let username = document.querySelector("#usernameInput")! as HTMLInputElement;
 let password = document.querySelector("#passwordInput")! as HTMLInputElement;
 let buttonLogin = document.querySelector("#buttonLogin")! as HTMLButtonElement;
-let text=document.getElementById("texttouser")! as HTMLDivElement;
-buttonLogin.onclick = async function () {
-    let[userFound,userId]= await send("login", [username.value, password.value])as [boolean,string];
-    console.log("user found:" + userFound);
-if(userFound){
-    localStorage.setItem("userId",userId);
-}
-
+let text = document.getElementById("texttouser")! as HTMLDivElement;
+buttonLogin.onclick =  function () {
+    // let [userFound, userId] = await send("login", [username.value, password.value]) as [boolean, string];
+    // console.log("user found:" + userFound);
+    // if (userFound) {
+    //     localStorage.setItem("userId", userId);
+    // }
+   window.location.href = "/website/pages/index.html";
 };
