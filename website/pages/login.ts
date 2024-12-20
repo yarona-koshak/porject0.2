f081c048e23e37062460ff8b66ddd8852066b681
yarona shneider <s332877679@ariel.ort.org.il>
2024-12-20 08:30:34 +0200

    שזדסגבכהענימחצל Co-authored-by: Nekto21-09 <Nekto21-09@users.noreply.github.com>

--- website/pages/login.ts
+++ website/pages/login.ts
@@ -4,15 +4,14 @@ let userNameInput = document.querySelector("#usernameInput")! as HTMLInputElemen
 let passwordInput = document.querySelector("#passwordInput")! as HTMLInputElement;
 let buttonLogin = document.querySelector("#buttonLogin")! as HTMLButtonElement;
 let text = document.getElementById("texttouser")! as HTMLDivElement;
-buttonLogin.onclick =  function () { 
-    let username=userNameInput.value;
-    
+buttonLogin.onclick = async function () { 
+    let [userFound, userId] = await send("login", [userNameInput.value, passwordInput.value]) as [boolean, string];
+    console.log("user found:" + userFound);
+    if (userFound) {
+        localStorage.setItem("userId", userId);
+    }
    window.location.href = "/website/pages/index.html";
 };
 
- // let [userFound, userId] = await send("login", [username.value, password.value]) as [boolean, string];
-    // console.log("user found:" + userFound);
-    // if (userFound) {
-    //     localStorage.setItem("userId", userId);
-    // }
+
     
--- website/pages/signUp.ts
+++ website/pages/signUp.ts
@@ -4,14 +4,6 @@ let passwordInput= document.querySelector("#passwordInput")! as HTMLInputElement
 let signupbutton = document.querySelector("#signupbutton")! as HTMLButtonElement;
 
 signupbutton.onclick=function(){
-    let username=userNameInput.value;
-    let password=passwordInput.value;
-
-if(!username||!password){
- alert("we need your user name and password");
- return;
-}
-let users=JSON.parse(localStorage.getItem("users")||"[]");
 
 
     window.location.href ="/website/pages/index.html";
