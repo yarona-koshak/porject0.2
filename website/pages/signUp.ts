896b25e6adc865be9d9ad3bc5072f313cc99082b
yarona-koshak <s332877679@ariel.ort.org.il>
2024-12-28 22:31:37 +0200

    massige in sign up

--- website/pages/signUp.ts
+++ website/pages/signUp.ts
@@ -3,10 +3,10 @@ import { send } from "../utilities";
 let userNameInput = document.querySelector("#userNameInput")! as HTMLInputElement;
 let passwordInput = document.querySelector("#passwordInput")! as HTMLInputElement;
 let signupbutton = document.querySelector("#signupbutton")! as HTMLButtonElement;
-let button = document.querySelector("#button")! as HTMLButtonElement;
+
 signupbutton.onclick = async function () {
     let userId = await send("signUp", [userNameInput.value, passwordInput.value]) as string;
     console.log(userId);
-    alert("hello hello");
+    alert("now you can play koshak says");
     window.location.href = "/website/pages/index.html";
 }
